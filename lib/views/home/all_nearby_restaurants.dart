b3d1bb4bfc1b3ab2a2117b9a0a6329e374a5f9a3
John Doe <johndoe@example.com>
2024-05-27 11:49:45 +0700

    NearBy RestaurantList

--- lib/views/home/all_nearby_restaurants.dart
+++ lib/views/home/all_nearby_restaurants.dart
@@ -1,8 +1,11 @@
+import 'package:demo/views/home/widgets/restaurant_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:demo/common/app_style.dart';
+import 'package:demo/common/back_ground_container.dart';
 import 'package:demo/common/reusable_text.dart';
 import 'package:demo/constants/constants.dart';
-import 'package:flutter/material.dart';
-import 'package:demo/common/app_style.dart';
-import 'package:demo/constants/constants.dart';
+import 'package:demo/constants/uidata.dart';
 
 class AllNearbyRestaurants extends StatelessWidget {
   const AllNearbyRestaurants({super.key});
@@ -10,15 +13,24 @@ class AllNearbyRestaurants extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kSecondary,
      appBar: AppBar(
-        elevation: 0.3,
-        backgroundColor: kOffWhite,
+        elevation: 0,
+        backgroundColor: kSecondary,
         title: ReusableText(text: "Nearby Restaurants", 
-        style: appStyle(13, kGray, FontWeight.w600)),
+        style: appStyle(13, kLightWhite, FontWeight.w600)),
       ),
-      body: const Center(
-        child: Text("All Nearby Restaurants"),
-      ),
+      body: BackGroundContainer(
+         color: Colors.white,
+        child: Padding(
+          padding:  EdgeInsets.all(12.h),
+          child: ListView(
+          children: List.generate(restaurants.length, (i) {
+            var restaurant = restaurants[i];
+            return RestaurantTile(restaurant: restaurant,);
+          }),
+              ),
+        ),),
     );
   }
 }
